db14f2a886a887848c65df8ba0d58dc38798e7f3
juliadobrovolska <julija.dobrovolskaja@privatbank.ua>
2025-05-22 13:50:09 +0300

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import {TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {provideHttpClient, withFetch} from '@angular/common/http';
+import {provideRouter} from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        provideHttpClient(withFetch()),  // Новый способ предоставления HttpClient
+        provideRouter([])               // Новый способ предоставления Router
+      ],
     }).compileComponents();
   });
 
--- src/app/user/user.service.spec.ts
+++ src/app/user/user.service.spec.ts
@@ -44,4 +44,3 @@ describe('UserService', () => {
     req.flush(dummyResponse);
   });
 });
-
