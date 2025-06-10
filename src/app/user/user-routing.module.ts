e670b08537f702fc9b6a942d9f1dc801c56cfb41
juliadobrovolska <julija.dobrovolskaja@privatbank.ua>
2025-06-10 11:57:16 +0300

    remove ssr from project

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -2,14 +2,23 @@ import {Routes} from '@angular/router';
 import {UserListComponent} from './components/user-list/user-list.component';
 import {userListResolver} from './resolvers/user-list.resolver';
 import {UserInfoComponent} from './components/user-info/user-info.component';
+import {RenderMode} from "@angular/ssr";
 
 
 export const userRoutes: Routes = [
-  {
-    path: '', component: UserListComponent,
-    resolve: {
-      userList: userListResolver
+    {
+        path: '', component: UserListComponent,
+        resolve: {
+            userList: userListResolver
+        }
+    },
+    {
+        path: ':id', component: UserInfoComponent,
+        data: {
+            prerender: {
+                noPrerender: true
+            }
+        }
+
     }
-  },
-  {path: ':id', component: UserInfoComponent}
 ]
